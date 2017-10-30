c8e7d0c2f7fdf06c2212aae656eadade4cb8343d
paragtyagi97 <32307064+paragtyagi97@users.noreply.github.com>
2017-10-30 23:43:26 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,36 +1,38 @@
 var express = require('express');
 var app = express();
-var port = process.env.PORT || 8080;
 var morgan = require('morgan');
-var mongoose = require('mongoose'); 
 var bodyParser = require('body-parser');
-
+var mongoose = require('mongoose');
 var router = express.Router();
-var appRoutes = require('./Server/Routes/route')(router);
+var appRoutes = require('./Server/Routes/api')(router);
+var spareRoutes = require('./Server/Routes/route')(router);
 var path = require('path');
 
 app.use(morgan('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true}));
-app.use(express.static(__dirname + '/Public'));
-app.use('/routes', appRoutes);
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(__dirname + '/public'));
+app.use('/api',appRoutes);
+app.use('/api',spareRoutes);
 
 
-mongoose.connect('mongodb://localhost:27017/page', function(err){
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost:27017/wellness', function(err){
     if (err) {
-        console.log('Not connected to the DB: ' + err);
+        console.log('not connected to database: '+ err);
     } else {
-        console.log('Succesfully connected to MongoDB');
+        console.log(' connected to database ');
     }
 });
 
-app.get('*', function(req, res){
-    res.sendFile(path.join(__dirname + '/Public/app/view/index.html'));
-});
-
-
-app.listen(port, function(){
-    console.log('running the server on port' + port);
+app.get('*', function(req,res) {
+    res.sendFile(path.join(__dirname + '/public/app/views/index.html'))
 
 });
 
+
+app.set('port', (process.env.PORT || 4000 ||  8080));
+
+app.listen(app.get('port'), function(){
+	console.log('Server started on port '+app.get('port'));
+});
