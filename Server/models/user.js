a5eb06e4e5f86610262106a2a482e2cdc99989ca
paragtyagi97 <32307064+paragtyagi97@users.noreply.github.com>
2017-11-05 14:17:35 +0530

    Update user.js

--- Server/models/user.js
+++ Server/models/user.js
@@ -1,60 +1,59 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
+var bcrypt = require('bcrypt-nodejs');
 
-var patientSchema = new Schema({
-    patient_id: {type: String,  required: true, unique: true},
-    password: {type: String, required: true},
-    firstName: {type: String, required: true},
-    middleName: String,
-    lastName: {type: String, required: true},
+
+var PatientSchema = new Schema({
+
+    name: {
+        firstName: {type: String, required: true},
+        middleName: String,
+        lastName: {type: String, required: true},
+        },
+
+    password: {type: String, required: true},   
     gender: {type: String, required: true},
-    dob: {type: Date, required: true},
+    dob: {type: String, required: true},
     contactNumber: {type: Number, required: true},
     email: {type: String, required: true, unique: true},
+
     address: {
-            houseNumber: String,
-            street: String,
+            line1: String,
+            line2: String,
             city: String,
+            district: String,
             state: String,
-            country: String,
-            zipCode: String 
+            pinCode: String, 
+            country: String
+            
             },
+
     occupation: String,
+
     patient_Details: {
                 patient_height: String,
                 patient_weight: String,
                 patient_maritaiStatus: String,
                 patient_Smoking_status: String,
                 patient_Drinking_status: String,
-                patient_allergy: String
+                patient_allergy: String,
+                patient_history: String
               },
+              
     responsible_person: {
                            name: String,
-                           number: [Number],
+                           number: Number,
                            relation: String,
                            address: String
-                        }          
+                        }                      
     
 });
 
 
-patientSchema.pre('save', function(next){
-    var patient= this;
-    bcrypt.hash(patient.password,null,null,function(err, hash){
-        if (err) return next(err);
-        patient.password = hash;
-        next();
-    });
-    bcrypt.hash(patient.contactNumber,null,null,function(err, hash){
-        if (err) return next(err);
-        patient.contactNumber = hash;
-        next();
-    });
 
-    });
-    
-   patientSchema.methods.comparePassword = function(password){
-        return bcrypt.compareSync(password, this.password);
-    };
 
-module.exports = mongoose.model('Patient', patientSchema);
+
+
+
+
+module.exports = mongoose.model('Patient', PatientSchema);
